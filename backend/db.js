3747efdbd5a5a5c1c71347306de6565d7e8b5791
KS171003 <kunalkomal1710@gmail.com>
2025-11-06 12:38:21 +0530

    fix: Enable SSL for MySQL connection

--- backend/db.js
+++ backend/db.js
@@ -6,8 +6,10 @@ dotenv.config(); // ✅ ensure env vars are loaded before connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: { "rejectUnauthorized": true }
 });
 
 db.connect((err) => {
